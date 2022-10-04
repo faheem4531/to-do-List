d75d8aa54e74fb7d39dca5deebc20d9c1277fecf
faheem4531 <aa0561085@gmail.com>
2022-10-04 15:27:44 +0500

    add functionalities

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleInputfield(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function handleInputArray() {
+    setItems(prevValue => {
+      return [...prevValue, inputText];
+    });
+    setInputText ("")
+  }
+
+  
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleInputfield} type="text" value={inputText} />
+        <button onClick={handleInputArray}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map(todoItem => <li>{todoItem}</li>)}
         </ul>
       </div>
     </div>
